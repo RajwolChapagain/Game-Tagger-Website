695cf410489e4cefeef294da0218e56a30339bab
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-06-11 17:26:44 -0400

    Render predicted tags

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Button from './Button'
 import { useState } from 'react'
 import image_placeholder from './assets/image_placeholder.png'
+import Text from './TagText'
 
 function App() {
+  const [tags, setTags] = useState([])
   const [iconSrc, setIconSrc] = useState(image_placeholder)
 
   const handleFile = async (file) => {
@@ -19,21 +21,30 @@ function App() {
       })
 
       const result = await response.json()
-      console.log('Response from FastAPI:', result.tags)
+      setTags(result.tags)
     } catch (error) {
       console.error('Upload failed:', error)
     }
   }
 
   return (
-    <div className="position-relative" style={{ height: '100vh' }}>
-      <Button
-        icon={iconSrc}
-        className="position-absolute start-50 translate-middle-x"
-        style={{ top: '20%' }}
-        onFileSelect={handleFile}
-      />
-    </div>
+    <>    
+      <div className="position-relative" style={{ height: '100vh' }}>
+        <Button
+          icon={iconSrc}
+          className="position-absolute start-50 translate-middle-x"
+          style={{ top: '20%' }}
+          onFileSelect={handleFile}
+        />
+      </div>
+      <div className="position-absolute translate-middle" style={{top: '75%', left: '50%'}}>
+        <div className="d-flex justify-content-start" style={{columnGap:'10px'}}>
+          {tags.map((tag) => (
+            <Text text={tag}/>
+          ))}
+        </div>
+      </div>
+    </> 
   )
 }
 
---
+++ src/TagText.jsx
@@ -0,0 +1,9 @@
+const Text = ({text}) => {
+    return (
+        <span className="px-2 py-1 me-2 rounded" style={{backgroundColor:"#4f7a9dff", color:"#7ac6f8ff"}}>
+            {text}
+        </span>
+    )
+}
+
+export default Text
