49b612d15abf54a33412f50a107ed5eaf0b99c1f
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-06-30 09:50:35 -0400

    Add description bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Button from './Button'
 import { useState } from 'react'
 import image_placeholder from './assets/image_placeholder.png'
 import Text from './TagText'
+import DescriptionBar from './DescriptionBar'
 
 function App() {
   const [tags, setTags] = useState([])
@@ -29,21 +30,27 @@ function App() {
 
   return (
     <>    
-      <div className="position-relative" style={{ height: '100vh' }}>
+<div style={{ position: 'relative', minHeight: '100vh' }}>
+      <div className="position-absolute start-50 translate-middle-x" style={{ top: '20%' }}>
         <Button
           icon={iconSrc}
-          className="position-absolute start-50 translate-middle-x"
-          style={{ top: '20%' }}
           onFileSelect={handleFile}
         />
       </div>
-      <div className="position-absolute translate-middle" style={{top: '75%', left: '50%'}}>
-        <div className="d-flex justify-content-start" style={{columnGap:'10px'}}>
-          {tags.map((tag) => (
-            <Text text={tag}/>
+
+      <div className="position-absolute translate-middle" style={{ top: '75%', left: '50%' }}>
+        <div className="d-flex justify-content-start" style={{ columnGap: '10px' }}>
+          {tags.map((tag, idx) => (
+            <Text key={idx} text={tag} />
           ))}
         </div>
       </div>
+
+      <div className="position-absolute translate-middle" style={{top:'90%', left:'50%'}}>
+        <DescriptionBar text="An AI model that tags games based on in-game screenshots. Currently supports 12 tags:  
+         • Platformer • Action • Casual • Adventure • TwoD • ThreeD • Simulation • Strategy • RPG • Puzzle • Horror • Sports "/>
+      </div>
+    </div>
     </> 
   )
 }
